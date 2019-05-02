e7dd2a08e5482ac5fd8dffe14ee55eacb2b0c974
duneoz <nicholas.charles.meyers@gmail.com>
2019-05-01 20:17:13 -0700

    added random number to target id and score total element

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,4 @@
-
- 
-$(document).ready(function() {
-
-    //create a function that generates a random number between 19-120
-    function getRandomArbitrary(min, max){
-        return Math.round(Math.random() * (max - min) + min);
-    }
-
-    console.log(getRandomArbitrary(19, 121));
-    
-    //create some variables
+//create variables for tracking target number, running total, red, orange, yellow, green, blue and purple stones
     var target = 0;
     var runningTotal = 0;
     var redStone = 0;
@@ -18,6 +7,24 @@ $(document).ready(function() {
     var greenStone = 0;
     var blueStone = 0;
     var purpleStone = 0;
+
+    //set initial values for html paragraph tags
+    document.getElementById('target').innerHTML = "Target: " + target;
+    document.getElementById('runningTotal').innerHTML = "Score: " + runningTotal;
+
+
+$(document).ready(function() {
+
+    //create a function that generates a random number between 19-120
+    function getRandomArbitrary(min, max){
+        return Math.round(Math.random() * (max - min) + min);
+    }
+
+    console.log(getRandomArbitrary(19, 121));
+
+    //add target to the page
+    target = getRandomArbitrary(19, 121);
+    document.getElementById('target').innerHTML = "Target: " + target;
 
 })  
 

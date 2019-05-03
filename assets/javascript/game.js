a5a13071f166c17f4a0a72439ef927c9c9a5caef
duneoz <nicholas.charles.meyers@gmail.com>
2019-05-02 18:38:54 -0700

    created functions for a lot of repeated code - reset stones, reset target, compare target to running total

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,22 +22,8 @@ $(document).ready(function() {
         return Math.round(Math.random() * (max - min) + min);
     };
 
-    //add target to the page
-    target = getRandomArbitrary(19, 120);
-    document.getElementById('target').innerHTML = "Target: " + target;
-
-    //generate random values for each stone
-        //create an array for a loop
-        //var array1 = [redStone, orangeStone, yellowStone, greenStone, blueStone, purpleStone];
-
-        //for loop that sets the value of each variable to a random number between 1 and 12
-        //not working quite right
-        // for (i = 0; i < array1.length; i++) {
-        //     array1[i] = getRandomArbitrary(1, 13);
-        //     console.log(array1[i] + "=" + i);
-        // };
-
-        //was not able to figure out the loop .. current syntax appears to change the variable name
+    //create a function for resetting stone values to call when running total is greater than, equal to target
+    function resetStones() {
         redStone = getRandomArbitrary(1, 12);
         console.log("redStone = " + redStone);
         orangeStone = getRandomArbitrary(1, 12);
@@ -50,8 +36,55 @@ $(document).ready(function() {
         console.log("blueStone = " + blueStone);
         purpleStone = getRandomArbitrary(1, 12);
         console.log("purpleStone = " + purpleStone);
+    };      
+
+    //create a function that resets the target
+    function resetTarget() {
+        target = getRandomArbitrary(19, 120);
+        document.getElementById('target').innerHTML = "Target: " + target;
+    }
+
+    //create a function that includes an if/then statement for equal to target, greater than target
+    function checkIt() {
+        if (target === runningTotal) {
+            alert("You defeated Thanos!");
+            wins = wins + 1;
+            document.getElementById('win').innerHTML = "Wins: " + wins;
+            runningTotal = 0;
+            document.getElementById('runningTotal').innerHTML = "Score: " + runningTotal;
+            target = getRandomArbitrary(19, 120);
+            document.getElementById('target').innerHTML = "Target: " + target;
+            resetStones();
+        } else if (target < runningTotal) {
+            alert ("Thanos defeated you! Snap!");
+            losses = losses + 1;
+            document.getElementById('loss').innerHTML = "Loss: " + losses;
+            runningTotal = 0;
+            document.getElementById('runningTotal').innerHTML = "Score: " + runningTotal;
+            target = getRandomArbitrary(19, 120);
+            document.getElementById('target').innerHTML = "Target: " + target;
+            resetStones();
+        }
+    }
+
+    //add target to the page
+    resetTarget();
+
+    //generate random values for each stone
+        //create an array for a loop
+                //var array1 = [redStone, orangeStone, yellowStone, greenStone, blueStone, purpleStone];
+
+        //for loop that sets the value of each variable to a random number between 1 and 12
+        //not working quite right
+                // for (i = 0; i < array1.length; i++) {
+                //     array1[i] = getRandomArbitrary(1, 13);
+                //     console.log(array1[i] + "=" + i);
+                // };
+
+    resetStones();
 
     //on click event for each of the stones that adds to the runningTotal variable
+        //this could probably be a loop for an array of stone variable names
 
         //on click of red stone add assigned value to running total, write to div
         $("#redStone").on("click", function(){
@@ -65,28 +98,68 @@ $(document).ready(function() {
             //if statement to compare target and runningtotal
                 //if target = running total, add 1 to win and reset
                 //if target < running total, add 1 to loss and reset
-                //if target > running total, do nothing, game continues
-
-            if (target === runningTotal) {
-                alert("You defeated Thanos!");
-                wins = wins + 1;
-                document.getElementById('win').innerHTML = "Wins: " + wins;
-                runningTotal = 0;
-                document.getElementById('runningTotal').innerHTML = "Score: " + runningTotal;
-                target = getRandomArbitrary(19, 120);
-            } else if (target < runningTotal) {
-                alert ("Thanos defeated you! Snap!");
-                losses = losses + 1;
-                document.getElementById('loss').innerHTML = "Loss: " + losses;
-                runningTotal = 0;
-                document.getElementById('runningTotal').innerHTML = "Score: " + runningTotal;
-                target = getRandomArbitrary(19, 120);
-                document.getElementById('target').innerHTML = "Target: " + target;
-            }
+            checkIt();           
         });
 
-    
+        $("#orangeStone").on("click", function(){
+            // console.log("got here");
+            runningTotal = runningTotal + orangeStone;
+            console.log("runningTotal = " + runningTotal);
 
+            //refresh running total div
+            document.getElementById('runningTotal').innerHTML = "Score: " + runningTotal;
+
+            //if statement
+            checkIt();
+        });
+
+        $("#yellowStone").on("click", function(){
+            // console.log("got here");
+            runningTotal = runningTotal + yellowStone;
+            console.log("runningTotal = " + runningTotal);
+
+            //refresh running total div
+            document.getElementById('runningTotal').innerHTML = "Score: " + runningTotal;
+
+            //if statement
+            checkIt();
+        });
+    
+        $("#greenStone").on("click", function(){
+            // console.log("got here");
+            runningTotal = runningTotal + greenStone;
+            console.log("runningTotal = " + runningTotal);
+
+            //refresh running total div
+            document.getElementById('runningTotal').innerHTML = "Score: " + runningTotal;
+
+            //if statement
+            checkIt();
+        });
+
+        $("#blueStone").on("click", function(){
+            // console.log("got here");
+            runningTotal = runningTotal + blueStone;
+            console.log("runningTotal = " + runningTotal);
+
+            //refresh running total div
+            document.getElementById('runningTotal').innerHTML = "Score: " + runningTotal;
+
+            //if statement to compare target and runningtotal
+            checkIt();
+        });
+
+        $("#purpleStone").on("click", function(){
+            // console.log("got here");
+            runningTotal = runningTotal + purpleStone;
+            console.log("runningTotal = " + runningTotal);
+
+            //refresh running total div
+            document.getElementById('runningTotal').innerHTML = "Score: " + runningTotal;
+
+            //if statement to compare target and runningtotal
+            checkIt();
+        });
 })  
 
 
